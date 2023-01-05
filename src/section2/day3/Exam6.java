5e6eacfc3dbfe6948990d093cfa5256bccb7115f
pyjavathon <ayeonyun391@gmail.com>
2023-01-05 16:37:44 +0900

    6번 소수 확인 해야 함

--- src/section2/day3/Exam6.java
+++ src/section2/day3/Exam6.java
@@ -3,6 +3,19 @@ package section2.day3;
 import java.util.Scanner;
 
 public class Exam6 {
+	
+	public int[] solution(int su, int[] arr) {
+		int [] dap = new int [3];
+		int temp = 0;
+		for(int i : arr) {
+			arr[temp] = Integer.parseInt(new StringBuilder(String.valueOf(i)).reverse().toString()); 
+			temp++;
+		}
+		
+		
+		for(int t: arr) System.out.println(t);
+			return dap;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,7 +23,15 @@ public class Exam6 {
 		
 		Exam6 ex6 = new Exam6();
 		
-		//
+		int su = scan.nextInt();
+		
+		int[] arr = new int[su];
+		
+		for(int i = 0; i < su; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		System.out.println(ex6.solution(su, arr));
 	}
 
 }
