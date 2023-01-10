ad9a5a3d970343b3c594490e9b0f58df6c442eec
pyjavathon <86497379+pyjavathon@users.noreply.github.com>
2023-01-10 23:31:58 +0900

    .

--- src/section2/day3/Exam6.java
+++ src/section2/day3/Exam6.java
@@ -1,7 +1,5 @@
 package section2.day3;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -48,74 +46,4 @@ public class Exam6 {
 			System.out.print(last + " ");
 		}
 			
-=======
-import java.util.Scanner;
-
-public class Exam6 {
-	
-	public int[] solution(int su, int[] arr) {
-		int [] dap = new int [3];
-		int temp = 0;
-		for(int i : arr) {
-			arr[temp] = Integer.parseInt(new StringBuilder(String.valueOf(i)).reverse().toString()); 
-			temp++;
-		}
-		
-		
-		for(int t: arr) System.out.println(t);
-			return dap;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		
-		Exam6 ex6 = new Exam6();
-		
-		int su = scan.nextInt();
-		
-		int[] arr = new int[su];
-		
-		for(int i = 0; i < su; i++) {
-			arr[i] = scan.nextInt();
-		}
-		
-		System.out.println(ex6.solution(su, arr));
->>>>>>> branch 'master' of https://github.com/pyjavathon/codingTest.git
-=======
-import java.util.Scanner;
-
-public class Exam6 {
-	
-	public int[] solution(int su, int[] arr) {
-		int [] dap = new int [3];
-		int temp = 0;
-		for(int i : arr) {
-			arr[temp] = Integer.parseInt(new StringBuilder(String.valueOf(i)).reverse().toString()); 
-			temp++;
-		}
-		
-		
-		for(int t: arr) System.out.println(t);
-			return dap;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		
-		Exam6 ex6 = new Exam6();
-		
-		int su = scan.nextInt();
-		
-		int[] arr = new int[su];
-		
-		for(int i = 0; i < su; i++) {
-			arr[i] = scan.nextInt();
-		}
-		
-		System.out.println(ex6.solution(su, arr));
->>>>>>> branch 'master' of https://github.com/pyjavathon/codingTest.git
-	}
-
 }
--- src/section2/day4/Exam7.java
+++ src/section2/day4/Exam7.java
@@ -2,5 +2,5 @@ package section2.day4;
 
 public class Exam7 {
 
-	//
+	/////
 }
