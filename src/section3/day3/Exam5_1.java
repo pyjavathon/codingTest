e980d9393a47fc76609a2f9d54cad7b33c7b728d
pyjavathon <86497379+pyjavathon@users.noreply.github.com>
2023-02-18 23:30:01 +0900

    .

--- src/section3/day3/Exam5_1.java
+++ src/section3/day3/Exam5_1.java
@@ -2,16 +2,19 @@ package section3.day3;
 
 import java.util.Scanner;
 
-public class Exam5 {
+public class Exam5_1 {
 	public int solution(int n) {
-		int dap=0, lt = 1, sum = 0;
+		int dap = 0, lt = 0, sum = 0;
+		int m = n/2 +1;
+		int []arr = new int[m];
+		for(int i = 0; i<m;i++) arr[i] = i+1;
 		
-		for(int rt = 1; rt < n; rt++) {
-			sum+=rt;
+		for(int rt = 0; rt<m;rt++) {
+			sum+=arr[rt];
 			if(sum==n) dap++;
 			while(sum>=n) {
-				sum-=lt++;
-				if(sum==n)dap++;
+				sum-=arr[lt++];
+				if(sum==n) dap++;
 			}
 		}
 		
@@ -19,9 +22,10 @@ public class Exam5 {
 	}
 
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
 		
-		Exam5 ex5 = new Exam5();
+		Exam5_1 ex5 = new Exam5_1();
 		
 		int n = scan.nextInt();
 		
